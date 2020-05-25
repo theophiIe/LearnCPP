3b4b45089a7109bd5786260bd28f3458fd244dfe
theophiIe <theopmolinatti@gmail.com>
2020-05-25 15:01:16 +0200

    Ajout de plusieurs tests avec des var de diff types

--- Exercices/TD1/EXO2/main.cpp
+++ Exercices/TD1/EXO2/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-void exo2_Q1(auto &a, auto &b)
+void chg(auto &a, auto &b)
 {
 	auto c{a};
 
@@ -10,17 +10,26 @@ void exo2_Q1(auto &a, auto &b)
 
 int main()
 {
-	exo1_Q1();
-	exo1_Q2();
-
 	auto a{6};
 	auto b{5};
+	
+	std::cout << "Avant changement : " << a << ", " << b << std::endl;
+	chg(a, b);
+	std::cout << "Apres changement : " << a << ", " << b << std::endl << std::endl;
+
 	auto c{"test"};
 	auto d{"TEST"};
 	
-	std::cout << a << " " << b << " " << c << " " << d << std::endl;
-	exo2_Q1(a, b);
-	exo2_Q1(c, d);
-	std::cout << a << " " << b << " " << c << " " << d << std::endl;
+	std::cout << "Avant changement : " << c << ", " << d << std::endl;
+	chg(c, d);
+	std::cout << "Apres changement : " << c << ", " << d << std::endl << std::endl;
+
+	auto e{5.4f};
+	auto f{4.6f};
+	
+	std::cout << "Avant changement : " << e << ", " << f << std::endl;
+	chg(e, f);
+	std::cout << "Apres changement : " << e << ", " << f << std::endl;
 	
 	return 0;
+}
