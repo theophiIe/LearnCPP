7b7ffc42216596677c1bce8e4f18fed60681d832
theophiIe <theomolinatti@gmail.com>
2020-07-25 16:06:20 +0200

    Optimisation du code

--- Projet/Calendrier/main.cpp
+++ Projet/Calendrier/main.cpp
@@ -2,50 +2,31 @@
 #include <ctime>
 #include <iomanip>
 
-int debutMois()
+int debutMois(tm *t)
 {
-    time_t tmm = time(0);
-    tm *t;
-    t = localtime(&tmm);
-
     return t->tm_wday - (t->tm_mday) % 7 + 1;
 }
 
-int finMois()
+int finMois(tm *t)
 {
-    time_t actuel = time(0);
-    tm *ltm = localtime(&actuel);
-
-    if(1 + ltm->tm_mon == 2)
+    if(1 + t->tm_mon == 2)
     {
-        return ((1900 + ltm->tm_year)%4 == 0) ? 29+1 : 28+1;
+        return ((1900 + t->tm_year)%4 == 0) ? 29+1 : 28+1;
     }
 
-    else if(1 + ltm->tm_mon < 8)
+    else if(1 + t->tm_mon < 8)
     {
-        return ((1 + ltm->tm_mon)%2 == 0) ? 30+1 : 31+1;
+        return ((1 + t->tm_mon)%2 == 0) ? 30+1 : 31+1;
     }
 
     else
     {
-        return ((1 + ltm->tm_mon)%2 == 0) ? 31+1 : 30+1;
+        return ((1 + t->tm_mon)%2 == 0) ? 31+1 : 30+1;
     }
 }
 
-void affichage()
+void premiereLigne(int &pos, int &jour)
 {
-    time_t tmm = time(0);
-    char* dt = ctime(&tmm);
-
-    std::cout << std::setfill('-') << std::setw(38) << "" << std::setfill(' ') << std::endl;
-	std::cout << std::setw(33) << dt;
-    std::cout << std::setfill('-') << std::setw(38) << "" << std::setfill(' ') << std::endl;
-    std::cout << "   lu   ma   me   je   ve   sa   di" << std::endl;
-    std::cout << std::setw(5*debutMois()) << "01";
-
-    auto jour{2};
-    auto pos{debutMois()};
-
     while(pos<7)
     {
         std::cout << std::setw(4) << "0" << jour;            
@@ -53,13 +34,13 @@ void affichage()
         jour++;
         pos++;
     }
+}
 
-    std::cout << std::endl;
-    
-    pos=1;
-    while(jour<finMois())
+void autreLigne(tm *t, int &pos, int &jour)
+{
+    while(jour<finMois(t))
     {
-        if(5*(debutMois()+1) < 5*7)
+        if(5*(debutMois(t)+1) < 5*7)
         {
             if(jour<10)
             {
@@ -74,15 +55,44 @@ void affichage()
         }
 
         if(pos%7==0)
+        {
             std::cout << std::endl;
+        }
+        
         jour++;
         pos++;
     }
 }
 
+void affichage(tm *t, time_t tmm)
+{
+    char* dt = ctime(&tmm);
+
+    std::cout << std::setfill('-') << std::setw(38) << "" << std::setfill(' ') << std::endl;
+	std::cout << std::setw(33) << dt;
+    std::cout << std::setfill('-') << std::setw(38) << "" << std::setfill(' ') << std::endl;
+    std::cout << "   lu   ma   me   je   ve   sa   di" << std::endl;
+    std::cout << std::setw(5*debutMois(t)) << "01";
+
+    auto jour{2};
+    auto pos{debutMois(t)};
+
+    premiereLigne(pos, jour);
+
+    std::cout << std::endl;
+    
+    pos=1;
+    autreLigne(t, pos, jour);
+
+    std::cout << std::endl;
+}
+
 int main()
 {
-    affichage();
-    std::cout << std::endl;
+    time_t tmm = time(0);
+    tm *t = localtime(&tmm);
+    
+    affichage(t, tmm);
+
 	return 0;
 }
